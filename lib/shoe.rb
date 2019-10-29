033557d07868168ec3b4d99b5cfc1388a84283c1
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-29 19:00:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,13 +6,12 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << unless 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  def brand
 
 end
